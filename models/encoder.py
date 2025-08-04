c4f19af039b5da6d9b3a1d27223f94ee37aab7d9
Khoa <22520675@gm.uit.edu.vn>
2025-08-04 13:16:11 +0700

    lets try again

--- models/encoder.py
+++ models/encoder.py
@@ -16,7 +16,7 @@ class TransformerTransducerEncoder(nn.Module):
     ):
         super().__init__()
         self.linear = nn.Linear(in_features=in_features, out_features=d_model)
-        # self.pe = PositionalEncoding(d_model)
+        self.pe = PositionalEncoding(d_model)
         
 
         self.layers = nn.ModuleList(
@@ -31,18 +31,18 @@ class TransformerTransducerEncoder(nn.Module):
             ]
         )
 
-        self.frontend = ConvolutionFrontEnd(
-            in_channels=1,
-            num_blocks=3,
-            num_layers_per_block=2,
-            out_channels=[8, 16, 32],
-            kernel_sizes=[3, 3, 3],
-            strides=[1, 2, 2],
-            residuals=[True, True, True],
-            activation=nn.ReLU,        
-            norm=nn.BatchNorm2d,            
-            dropout=0.1,
-        )
+        # self.frontend = ConvolutionFrontEnd(
+        #     in_channels=1,
+        #     num_blocks=3,
+        #     num_layers_per_block=2,
+        #     out_channels=[8, 16, 32],
+        #     kernel_sizes=[3, 3, 3],
+        #     strides=[1, 2, 2],
+        #     residuals=[True, True, True],
+        #     activation=nn.ReLU,        
+        #     norm=nn.BatchNorm2d,            
+        #     dropout=0.1,
+        # )
 
     def forward(
         self,
@@ -50,18 +50,19 @@ class TransformerTransducerEncoder(nn.Module):
         mask: torch.Tensor = None,
     ) -> torch.Tensor:
         
-        x = x.unsqueeze(1)  # [batch, channels, time, features]
-        # print("x shape before frontend:", x.shape)  # [batch, 1, time, features]
-        x, mask = self.frontend(x, mask)  # [batch, channels, time, features]
-        # print("x shape after frontend:", x.shape)
-        x = x.transpose(1, 2).contiguous()   # batch, time, channels, features
-        x = x.reshape(x.shape[0], x.shape[1], -1) # [batch, time, C * features]
+        # x = x.unsqueeze(1)  # [batch, channels, time, features]
+        # # print("x shape before frontend:", x.shape)  # [batch, 1, time, features]
+        # x, mask = self.frontend(x, mask)  # [batch, channels, time, features]
+        # # print("x shape after frontend:", x.shape)
+        # x = x.transpose(1, 2).contiguous()   # batch, time, channels, features
+        # x = x.reshape(x.shape[0], x.shape[1], -1) # [batch, time, C * features]
 
 
         lengths = torch.sum(mask, dim=1)
         out = self.linear(x)  # (batch, seq_len, d_model)
+        out = self.pe(out)  # (batch, seq_len, d_model)
         mask = mask.unsqueeze(1).unsqueeze(1)
-
+        
         for layer in self.layers:
             out = layer(out, mask)
         
